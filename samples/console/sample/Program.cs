8c7505d32122b83cda9f32ede492bb40d7856942
bert <btemme@hotmail.com>
2017-09-08 20:09:35 +0200

    initial

---
+++ samples/console/sample/Program.cs
@@ -0,0 +1,72 @@
+﻿using gpxbelt_cs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Tiles.Tools;
+
+namespace sample
+{
+    class Program
+    {
+        public static List<Tile> BboxToTiles(double[] bbox, int level)
+        {
+            var minTile = Tilebelt.PointToTile(bbox[0], bbox[1], level);
+            var maxTile = Tilebelt.PointToTile(bbox[2], bbox[3], level);
+            var result = new List<Tile>();
+            for (var lon = minTile.X; lon <= maxTile.X; lon++)
+            {
+                for (var lat = maxTile.Y; lat <= minTile.Y; lat++)
+                {
+                    var t = new Tile(lon, lat, level);
+                    result.Add(t);
+                }
+            }
+            return result;
+        }
+
+        public static List<Tile> GetIntersectingTiles(GpxDocument doc, int level)
+        {
+            var result = new List<Tile>();
+            var bounds = doc.Bounds();
+            var tiles = BboxToTiles(bounds, 14);
+
+            foreach (var tile in tiles)
+            {
+                if (doc.Intersects(tile.Bounds()))
+                {
+                    result.Add(tile);
+                }
+            }
+            return result;
+        }
+
+        public static List<Tile> GetIntersectingTiles(string datadir)
+        {
+            var files = Directory.GetFiles("data");
+            var intersectingtiles = new List<Tile>();
+
+            foreach (var file in files)
+            {
+                var gpxDocument = new GpxDocument(file);
+                var tiles = GetIntersectingTiles(gpxDocument, 14);
+
+                foreach (var tile in tiles)
+                {
+                    if (!intersectingtiles.Contains(tile))
+                    {
+                        intersectingtiles.Add(tile);
+                    };
+                }
+            }
+            return intersectingtiles;
+
+        }
+
+        static void Main(string[] args)
+        {
+            var intersectingtiles = GetIntersectingTiles("data");
+            Console.WriteLine("Tiles: " + intersectingtiles.Count);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/GpxDocument.cs
@@ -0,0 +1,59 @@
+﻿using System.Xml;
+
+namespace gpxbelt_cs
+{
+    public class GpxDocument
+    {
+        private XmlDocument gpxDoc;
+        private XmlNamespaceManager nsmgr;
+
+        public GpxDocument(string file)
+        {
+            gpxDoc = new XmlDocument();
+            nsmgr = new XmlNamespaceManager(gpxDoc.NameTable);
+            nsmgr.AddNamespace("x", "http://www.topografix.com/GPX/1/1");
+            gpxDoc.Load(file);
+        }
+
+        public int Count
+        {
+            get
+            {
+                XmlNodeList nl = gpxDoc.SelectNodes("//x:trkpt", nsmgr);
+                return nl.Count;
+            }
+        }
+
+        public double[] Bounds()
+        {
+            double x0 = double.MaxValue, y0 = double.MaxValue, x1 =double.MinValue, y1 = double.MinValue;
+            XmlNodeList nl = gpxDoc.SelectNodes("//x:trkpt", nsmgr);
+            foreach (XmlNode n in nl)
+            {
+                var lat = double.Parse(n.Attributes["lat"].Value);
+                var lon = double.Parse(n.Attributes["lon"].Value);
+                x0 = (lon < x0 ? lon : x0);
+                x1 = (lon > x1 ? lon : x1);
+                y0 = (lat < y0 ? lat : y0);
+                y1 = (lat > y1 ? lat : y1);
+            }
+            return new double[4] { x0, y0, x1, y1 };
+        }
+
+        public bool Intersects(double[] bounds)
+        {
+            XmlNodeList nl = gpxDoc.SelectNodes("//x:trkpt", nsmgr);
+            foreach (XmlNode n in nl)
+            {
+                var lat = double.Parse(n.Attributes["lat"].Value);
+                var lon = double.Parse(n.Attributes["lon"].Value);
+
+                if (lon >= bounds[0] && lon <= bounds[2] && lat >= bounds[1] && lat <= bounds[3])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
